257b057b70d16533b45a102186155d53224cc1f1
richardh <r.m.van.hees@sron.nl>
2017-04-07 21:57:50 +0200

    biweight: option to convert spread to standard deviation

--- pys5p/biweight.py
+++ pys5p/biweight.py
@@ -15,7 +15,10 @@ from __future__ import division
 
 import numpy as np
 
-def biweight(data, axis=None, spread=False):
+def __corr_std(nval):
+    return 0.9909 + (0.5645 + 2.805 / nval) / nval
+
+def biweight(data, axis=None, spread=False, std=False):
     """
     Calculate Tukey's biweight.
     Implementation based on Eqn. 7.6 and 7.7 in the SWIR OCAL ATBD.
@@ -27,13 +30,17 @@ def biweight(data, axis=None, spread=False):
     axis   :   int, optional
        Axis along which the biweight medians are computed.
     spread :   bool
-       If True, then compute the biweight spread
+       If True, then return the biweight spread
+    std :   bool
+       If True, then return an approximation of the unbiased standard deviation
     Returns
     -------
     out    :   ndarray
        biweight median and biweight spread if function argument "spread" is True
-
     """
+    if std:
+        spread = True
+
     if axis is None:
         xx = data[np.isfinite(data)]
         med_xx = np.median(xx)
@@ -51,14 +58,16 @@ def biweight(data, axis=None, spread=False):
                 sbi = np.sum(deltas ** 2 * (1 - umn) ** 4)
                 sbi /= np.sum((1 - umn) *  (1 - 5 * umn)) ** 2
                 sbi = np.sqrt(len(xx) * sbi)
+                if std:
+                    sbi *= __corr_std(len(xx))
     else:
         med_xx = np.nanmedian(data, axis=axis)
         xbi = med_xx
         sbi = np.zeros_like(med_xx)
 
         deltas = data - np.expand_dims(med_xx, axis=axis)
-
         med_dd = np.nanmedian(np.abs(deltas), axis=axis)
+
         indices = (med_dd == 0)
         if not np.all(indices):
             med_dd[indices] = 1.  # dummy value
@@ -71,7 +80,11 @@ def biweight(data, axis=None, spread=False):
                 len_xx = np.sum(np.isfinite(data), axis=axis)
                 buff = np.sum(deltas ** 2 * (1 - umn) ** 4, axis=axis)
                 buff /= np.sum((1 - umn) *  (1 - 5 * umn), axis=axis) ** 2
-                sbi[~indices] = np.sqrt(len_xx * buff)[~indices]
+                if std:
+                    buff = __corr_std(len_xx) * np.sqrt(len_xx * buff)
+                else:
+                    buff = np.sqrt(len_xx * buff)
+                sbi[~indices] = buff[~indices]
 
     if spread:
         return (xbi, sbi)
