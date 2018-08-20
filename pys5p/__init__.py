7dd17b42a36685353d89b375f5e273a304600107
richardh <r.m.van.hees@sron.nl>
2018-08-20 13:26:06 +0200

    h5py: ignore of warnings no longer needed

--- pys5p/__init__.py
+++ pys5p/__init__.py
@@ -2,8 +2,6 @@ __all__ = ['biweight', 'ckd_io', 'error_propagation', 'get_data_dir',
            'icm_io', 'l1b_io', 'ocm_io', 's5p_msm', 's5p_plot',
            'sron_colormaps', 'swir_region', 'version']
 
-import warnings
-
 from . import biweight
 from . import error_propagation
 from . import get_data_dir
@@ -11,12 +9,10 @@ from . import sron_colormaps
 from . import swir_region
 from . import version
 
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=FutureWarning)
-    from . import ckd_io
-    from . import icm_io
-    from . import l1b_io
-    from . import ocm_io
+from . import ckd_io
+from . import icm_io
+from . import l1b_io
+from . import ocm_io
 
-    from . import s5p_msm
-    from . import s5p_plot
+from . import s5p_msm
+from . import s5p_plot
