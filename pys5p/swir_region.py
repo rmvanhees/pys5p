cd4c43c08c05a1ee52525d012948e4064fcc23ed
richardh <r.m.van.hees@sron.nl>
2019-01-07 13:51:20 +0100

    swir_region.py: updated documentation, replaced keyword region by mode

--- pys5p/swir_region.py
+++ pys5p/swir_region.py
@@ -1,48 +1,51 @@
 """
-This file is part of pyS5pMon
+This file is part of pyS5p
 
-https://git.sron.nl/tropomi_icm.git
+https://github.com/rmvanhees/pys5p.git
 
-Return SWIR detector mask with the 'illuminated' or 'level2' region
+There are two definitions of the usable area on the SWIR detector:
+ - 'illuminated' - detector area illuminated by external sources,
+ defined as an rectangular area where the signal is at least 50% of the
+ maximum signal. Coordinates: rows [11:228], columns [16:991].
+ - 'level2' - a smaller area used in official SWIR level 1B (ir)radiance
+ products. Coordinates: rows [12:227], columns [20:980].
+
+Note row 257 of the SWIR detector is neglected
 
 Copyright (c) 2017 SRON - Netherlands Institute for Space Research
    All Rights Reserved
 
 License:  BSD-3-Clause
 """
-
 import numpy as np
 
 
-def mask(region='illuminated'):
+def coords(mode='illuminated'):
     """
-    Return mask with the illuminated region has boolean value True
+    Return slice defining the illuminated region on the SWIR detector
 
     Parameters
     ----------
-    region  :   string, optional
-      'illuminated' - detector area illuminated by external sources,
-      defined as the area where the signal is at least 50% of the main signal
-      'level2' - a smaller area used for (ir)radiance products given to level 2
+    mode  :   {'illuminated', 'level2'}, optional
+       default is 'illuminated'
     """
-    res = np.full((256, 1000), False)
-    res[coords(region)] = True
-
-    return res
-
-
-def coords(region='illuminated'):
-    """
-    Return slice defining the illuminated region
-
-    Parameters
-    ----------
-    region  :   string, optional
-      'illuminated' - detector area illuminated by external sources,
-      defined as the area where the signal is at least 50% of the main signal
-      'level2' - a smaller area used for (ir)radiance products given to level 2
-    """
-    if region == 'level2':
+    if mode == 'level2':
         return np.s_[12:227, 20:980]
 
     return np.s_[11:228, 16:991]
+
+
+def mask(mode='illuminated'):
+    """
+    Return mask of the illuminated region, where the value of the illuminated
+    pixels are set to True.
+
+    Parameters
+    ----------
+    mode  :   {'illuminated', 'level2'}, optional
+       default is 'illuminated'
+    """
+    res = np.full((256, 1000), False)
+    res[coords(mode)] = True
+
+    return res
