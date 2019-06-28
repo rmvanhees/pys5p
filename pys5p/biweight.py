930ae2c109eb9885e79935625c5f71699a734f11
Richard van Hees <r.m.van.hees@sron.nl>
2019-06-28 16:42:51 +0200

    biweight.py: rewrote implementation, much faster without multiprocessing

--- pys5p/biweight.py
+++ pys5p/biweight.py
@@ -3,9 +3,9 @@ This file is part of pyS5p
 
 https://github.com/rmvanhees/pys5p.git
 
-Implement Tukey's biweight algorithm
+Implementation of the Tukey's biweight algorithm
 
-Copyright (c) 2016 SRON - Netherlands Institute for Space Research
+Copyright (c) 2019 SRON - Netherlands Institute for Space Research
    All Rights Reserved
 
 License:  BSD-3-Clause
@@ -14,54 +14,75 @@ import numpy as np
 
 
 # ----- local functions -------------------------
-def __biweight_median(data):
+def __biweight(data, spread=False):
     """
-    Calculate only biweight median
+    Calculate biweight median and spread, for whole dataset
     """
     mask = np.isfinite(data)
     if np.all(~mask):
+        if spread:
+            return (np.nan, 0.)
+
         return np.nan
 
-    xx = data[mask]
-    med_xx = np.median(xx)
-    deltas = xx - med_xx
-    med_dd = np.median(np.abs(deltas))
-    if med_dd == 0:
-        return med_xx
+    # calculate biweight median
+    data_median = np.nanmedian(data)
+    delta = data - data_median
+    delta_median = np.nanmedian(np.abs(delta))
+    if delta_median == 0.:
+        if spread:
+            return (data_median, 0.)
 
-    wmx = np.maximum(0, 1 - (deltas / (6 * med_dd)) ** 2) ** 2
+        return data_median
 
-    return med_xx + np.sum(wmx * deltas) / np.sum(wmx)
+    wmx = np.clip(1 - (delta / (6 * delta_median)) ** 2, 0, None) ** 2
+    biweight_median = data_median + np.nansum(wmx * delta) / np.nansum(wmx)
+    if not spread:
+        return biweight_median
+
+    # calculate biweight spread
+    umn = np.clip((delta / (9 * delta_median)) ** 2, None, 1)
+    buff = np.nansum(delta ** 2 * (1 - umn) ** 4)
+    buff /= np.nansum((1 - umn) * (1 - 5 * umn)) ** 2
+    biweight_spread = np.sqrt(np.sum(mask) * buff)
+
+    return (biweight_median, biweight_spread)
 
 
-def __biweight(data):
+def __biweight_axis(data, axis, spread=False):
     """
-    Calculate biweight median and spread
+    Calculate only biweight median, along a given axis
     """
     mask = np.isfinite(data)
-    if np.all(~mask):
-        return (np.nan, 0.)
+    all_nan = np.all(np.isnan(data), axis=axis)
 
-    xx = data[mask]
-    med_xx = np.median(xx)
-    deltas = xx - med_xx
-    med_dd = np.median(np.abs(deltas))
-    if med_dd == 0:
-        return (med_xx, 0.)
+    # calculate biweight median
+    data_median = np.nanmedian(data, axis=axis, keepdims=True)
+    delta = data - data_median
+    delta_median = np.nanmedian(np.abs(delta), axis=axis, keepdims=True)
+    _mm = delta_median != 0.
+    delta_median[~_mm] = np.nan
 
-    wmx = np.maximum(0, 1 - (deltas / (6 * med_dd)) ** 2) ** 2
-    xbi = med_xx + np.sum(wmx * deltas) / np.sum(wmx)
+    wmx = np.clip(1 - (delta / (6 * delta_median)) ** 2, 0, None) ** 2
+    biweight_median = np.squeeze(data_median)
+    _mm = np.squeeze(_mm) & ~all_nan
+    biweight_median[_mm] += \
+        np.nansum(wmx * delta, axis=axis)[_mm] / np.nansum(wmx, axis=axis)[_mm]
+    if not spread:
+        return biweight_median
 
-    umn = np.minimum(1, (deltas / (9 * med_dd)) ** 2)
-    sbi = np.sum(deltas ** 2 * (1 - umn) ** 4)
-    sbi /= np.sum((1 - umn) * (1 - 5 * umn)) ** 2
-    sbi = np.sqrt(len(xx) * sbi)
+    # calculate biweight spread
+    umn = np.clip((delta / (9 * delta_median)) ** 2, None, 1)
+    buff = np.nansum(delta ** 2 * (1 - umn) ** 4, axis=axis)[_mm]
+    buff /= np.nansum((1 - umn) * (1 - 5 * umn), axis=axis)[_mm] ** 2
+    biweight_spread = np.full(biweight_median.shape, np.nan)
+    biweight_spread[_mm] = np.sqrt(np.sum(mask, axis=axis)[_mm] * buff)
 
-    return (xbi, sbi)
+    return (biweight_median, biweight_spread)
 
 
 # ----- main function -------------------------
-def biweight(data, axis=None, cpu_count=1, spread=False):
+def biweight(data, axis=None, spread=False):
     """
     Calculate Tukey's biweight.
     Implementation based on Eqn. 7.6 and 7.7 in the SWIR OCAL ATBD.
@@ -73,24 +94,18 @@ def biweight(data, axis=None, cpu_count=1, spread=False):
     axis   :   int, optional
        axis along which the biweight medians are computed.
         - Note that axis will be ignored when data is a 1-D array.
-    cpu_count : int, optional
-       specify number of threads to be used. Default is None
-        - Note Multiprocessing is used only when axis is not None.
     spread :   bool, optional
-       if True, then return the biweight spread.
+       if True, then return also the biweight spread.
 
     Returns
     -------
     out    :   ndarray
        biweight median and biweight spread if function argument "spread" is True
     """
-    from multiprocessing import Pool
+    import warnings
 
     if axis is None or data.ndim == 1:
-        if spread:
-            return __biweight(data)
-
-        return __biweight_median(data)
+        return __biweight(data, spread)
 
     # only a single axis!
     if not isinstance(axis, int):
@@ -98,21 +113,6 @@ def biweight(data, axis=None, cpu_count=1, spread=False):
     if not 0 <= axis < data.ndim:
         raise ValueError('axis out-of-range')
 
-    # prepare data for multiprocessing
-    res_size = data.size // data.shape[axis]
-    tmp = np.moveaxis(data, axis, 0)        # returns a numpy view
-    shape = tmp.shape
-    buff = tmp.reshape(shape[0], res_size)  # returns a numpy view
-    ins = [buff[:, ii] for ii in range(res_size)]
-
-    with Pool(cpu_count) as pool:
-        if spread:
-            outs = pool.map(__biweight, ins)
-        else:
-            outs = pool.map(__biweight_median, ins)
-
-    if spread:
-        res = np.array(outs).reshape(shape[1:] + (2,))
-        return (res[..., 0], res[..., 1])
-
-    return np.array(outs).reshape(shape[1:])
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', r'All-NaN (slice|axis) encountered')
+        return __biweight_axis(data, axis, spread)
