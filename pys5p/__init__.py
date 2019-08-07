832c9e6da1552990fdd44572fbafe7db75e3c2eb
Richard van Hees <r.m.van.hees@sron.nl>
2019-08-07 13:32:38 +0200

    __init__.py: added module attribute __version__

--- pys5p/__init__.py
+++ pys5p/__init__.py
@@ -1,33 +1,22 @@
-"""
-This file is part of pys5p
+# This file is part of pys5p
+#
+# https://github.com/rmvanhees/pys5p.git
+#
+# Copyright (c) 2017 SRON - Netherlands Institute for Space Research
+#   All Rights Reserved
+# 
+# License:  BSD-3-Clause
 
-https://github.com/rmvanhees/pys5p.git
+""" The pys5p package contains software to read S5p Tropomi L1B products.
+    And contains plotting routines to display your data beautifully."""
 
-Copyright (c) 2017 SRON - Netherlands Institute for Space Research
-   All Rights Reserved
+from .version import version as __version__
 
-License:  BSD-3-Clause
-"""
-__all__ = ['biweight', 'ckd_io', 'error_propagation', 'get_data_dir',
-           'icm_io', 'l1b_io', 'lv2_io', 'ocm_io',
-           's5p_geoplot', 's5p_msm', 's5p_plot',
-           'sron_colormaps', 'swir_region', 'swir_texp',
-           'version']
+from .biweight import biweight
+from .get_data_dir import get_data_dir
+from .swir_texp import swir_exp_time
 
-from . import biweight
+from . import ckd_io, icm_io, l1b_io, lv2_io, ocm_io
 from . import error_propagation
-from . import get_data_dir
-from . import sron_colormaps
+from . import sron_colormaps, s5p_msm, s5p_plot #, s5p_geoplot
 from . import swir_region
-from . import swir_texp
-from . import version
-
-from . import ckd_io
-from . import icm_io
-from . import l1b_io
-from . import lv2_io
-from . import ocm_io
-
-from . import s5p_msm
-from . import s5p_plot
-# from . import s5p_geoplot
