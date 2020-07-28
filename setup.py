5ef42d2fd9cf525d86ca2d8c4ebc16144764ca21
Richard van Hees <r.m.van.hees@sron.nl>
2020-07-28 11:17:47 +0200

    setup.py: preparing for release of version 1.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(name='pys5p',
       use_scm_version={"root": ".",
                        "relative_to": __file__,
-                       "fallback_version": "1.0.1"},
+                       "fallback_version": "1.0.4"},
       setup_requires=['setuptools_scm'],
       description='Software package to access S5p Tropomi data products',
       long_description=readme(),
