f134ed5cf9ef91216fa7e04df1a91679c9d7c9cc
Richard van Hees <r.m.van.hees@sron.nl>
2020-08-12 12:43:29 +0200

    Added function to obtain version of pys5p package

---
+++ src/pys5p/version.py
@@ -0,0 +1,20 @@
+"""
+This file is part of pys5p
+
+https://github.com/rmvanhees/pys5p.git
+
+Copyright (c) 2020 SRON - Netherlands Institute for Space Research
+   All Rights Reserved
+
+License:  BSD-3-Clause
+"""
+from pys5p import __version__
+
+def get(full=False):
+    """
+    Returns software version as obtained from git
+    """
+    if full:
+        return __version__
+
+    return __version__.split('+')[0]
