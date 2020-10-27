c56454d4932bb56eac9a1792e67c55c6f4f8b770
Richard van Hees <r.m.van.hees@sron.nl>
2020-10-27 16:42:25 +0100

    s5p_geoplot.py: fixed serveral bugs and improved layout of the figures

--- src/pys5p/s5p_geoplot.py
+++ src/pys5p/s5p_geoplot.py
@@ -13,9 +13,6 @@ License:  BSD-3-Clause
 """
 from pathlib import PurePath
 
-import matplotlib as mpl
-import numpy as np
-
 try:
     import cartopy.crs as ccrs
     import cartopy.feature as cfeature
@@ -23,14 +20,15 @@ try:
                                        LATITUDE_FORMATTER)
 except Exception as exc:
     raise RuntimeError('This module require module Cartopy') from exc
-from matplotlib import pyplot as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib.patches import Polygon
+import numpy as np
 import shapely.geometry as sgeom
 
-from .lib.plotlib import FIGinfo
-from .s5p_msm import S5Pmsm
-from .tol_colors import tol_cmap
+from .lib.plotlib import check_data2d, FIGinfo
+# from .s5p_msm import S5Pmsm
+from .tol_colors import tol_cmap, tol_cset
 
 
 # - main function ----------------------------------
@@ -96,27 +94,28 @@ class BetterTransverseMercator(ccrs.Projection):
 
 
 # --------------------------------------------------
-class S5Pgeoplot():
+class S5Pgeoplot:
     """
     Generate figure(s) for the SRON Tropomi SWIR monitor website or MPC reports
 
     Attributes
     ----------
-    add_info : bool
-    aspect : int
-    data : ndarray
+    cset : dict
+       Dictionary with RGB colors of water, land, grid and satellite
     filename : string
-    method : string
+       Name of the PDF or PNG file
 
     Methods
     -------
-    def close_page(fig, fig_info):
-    def close():
-    def add_copyright(axx):
-    def draw_geo_tiles(lons, lats, sequence=None, title=None, fig_info=None)
-    def draw_geo_subsat(lons, lats, title=None, fig_info=None)
-    def draw_geo_msm(gridlon, gridlat, msm_in, vperc=None, vrange=None,
-                     whole_globe=False, title=None, fig_info=None)
+    close():
+       Close PNG or (multipage) PDF document.
+    draw_geo_msm(gridlon, gridlat, msm_in, vperc=None, vrange=None,
+                 whole_globe=False, title=None, fig_info=None)
+       Display measurement data projected with TransverseMercator.
+    draw_geo_subsat(lons, lats, title=None, fig_info=None)
+       Display sub-satellite footprints projected with TransverseMercator.
+    draw_geo_tiles(lons, lats, sequence=None, title=None, fig_info=None)
+       Display measurement footprints projected with TransverseMercator.
 
     Notes
     -----
@@ -137,7 +136,6 @@ class S5Pgeoplot():
              Default: 'Monitor report on Tropomi SWIR instrument'
         """
         self.filename = figname
-
         if PurePath(figname).suffix.lower() == '.pdf':
             self.__pdf = PdfPages(figname)
             # add annotation
@@ -150,15 +148,15 @@ class S5Pgeoplot():
         else:
             self.__pdf = None
 
+        self.__cmap = None
+        self.__zunit = None
+
         # define colors
         self.cset = {
             'water': '#ddeeff',
             'land': '#e1c999',
             'grid': '#bbbbbb',
             's5p': '#ee6677'}
-
-        #self.data = None
-        #self.method = None
 
     def __repr__(self):
         pass
@@ -170,10 +168,9 @@ class S5Pgeoplot():
         # add save figure
         if self.__pdf is None:
             plt.savefig(self.filename)
-            # plt.savefig(self.filename, transparant=True)
             plt.close(fig)
         else:
-            self.__pdf.savefig(transparant=True)
+            self.__pdf.savefig()
 
     def close(self):
         """
@@ -186,6 +183,51 @@ class S5Pgeoplot():
         plt.close('all')
 
     # --------------------------------------------------
+    def set_cmap(self, cmap):
+        """
+        Define alternative color-map to overrule the default
+
+        Parameter
+        ---------
+         cmap :  matplotlib color-map
+        """
+        self.__cmap = cmap
+
+    def unset_cmap(self):
+        """
+        Unset user supplied color-map, and use default color-map
+        """
+        self.__cmap = None
+
+    def get_cmap(self):
+        """
+        Returns matplotlib colormap
+        """
+        if self.__cmap is not None:
+            return self.__cmap
+
+        return tol_cmap('rainbow_PuRd')
+
+    def set_zunit(self, units):
+        """
+        Provide units of data to be displayed
+        """
+        self.__zunit = units
+
+    def unset_zunit(self):
+        """
+        Unset user supplied unit definition of data
+        """
+        self.__zunit = None
+
+    @property
+    def zunit(self):
+        """
+        Returns value of zunit
+        """
+        return self.__zunit
+
+    # -------------------------
     @staticmethod
     def __add_copyright(axx):
         """
@@ -242,22 +284,23 @@ class S5Pgeoplot():
         glx.yformatter = LATITUDE_FORMATTER
 
     # --------------------------------------------------
-    def draw_geo_tiles(self, lons, lats, *, sequence=None,
-                       title=None, fig_info=None):
+    def draw_geo_subsat(self, lons, lats, *,
+                        title=None, sub_title=None, fig_info=None):
         """
-        Display footprints projected with (beter) TransverseMercator
+        Display sub-satellite coordinates projected with TransverseMercator
 
         Parameters
         ----------
-        lats         :  ndarray
+        lats :  ndarray
            Latitude coordinates
-        lons         :  ndarray
+        lons :  ndarray
            Longitude coordinates
-        sequence     :  list
-           Indices to footprints to be drawn by polygons
-        title      :  string
+        title :  string, optional
            Title of the figure. Default is None
            Suggestion: use attribute "title" of data-product
+        sub_title :  string, optional
+           Sub-title of the figure. Default is None
+           Suggestion: use attribute "comment" of data-product
         fig_info :  FIGinfo, optional
            OrderedDict holding meta-data to be displayed in the figure
 
@@ -276,30 +319,95 @@ class S5Pgeoplot():
         lon_0 = np.around(np.mean(lons), decimals=-1)
 
         # inititalize figure
-        fig = plt.figure(figsize=(15, 10))
+        fig = plt.figure(figsize=(12.5, 9))
         if title is not None:
-            ypos = 1 - 0.25 / fig.get_figheight()
+            ypos = 1 - 0.35 / fig.get_figheight()
             fig.suptitle(title, fontsize='x-large',
                          position=(0.5, ypos), horizontalalignment='center')
 
         # draw worldmap
         axx = plt.axes(projection=BetterTransverseMercator(
-            central_longitude=lon_0, orientation=1,
+            central_longitude=lon_0, orientation=0,
             globe=ccrs.Globe(ellipse='sphere')))
-        self.__draw_worldmap(axx)
+        if sub_title is not None:
+            axx.set_title(sub_title, fontsize='large')
+        self.__draw_worldmap(axx, whole_globe=True)
+
+        # draw sub-satellite spot(s)
+        axx.scatter(lons, lats, 4, transform=ccrs.PlateCarree(),
+                    marker='o', color=self.cset['s5p'])
+
+        self.__add_copyright(axx)
+        fig_info.add('lon0', lon_0)
+        self.__add_fig_box(fig, fig_info)
+        self.__close_this_page(fig)
+
+    # --------------------------------------------------
+    def draw_geo_tiles(self, lons, lats, *, sequence=None,
+                       title=None, sub_title=None, fig_info=None):
+        """
+        Display footprints projected with (beter) TransverseMercator
+
+        Parameters
+        ----------
+        lats :  ndarray
+           Latitude coordinates
+        lons :  ndarray
+           Longitude coordinates
+        sequence :  list
+           Indices to footprints to be drawn by polygons
+        title :  string, optional
+           Title of the figure. Default is None
+           Suggestion: use attribute "title" of data-product
+        sub_title :  string, optional
+           Sub-title of the figure. Default is None
+           Suggestion: use attribute "comment" of data-product
+        fig_info :  FIGinfo, optional
+           OrderedDict holding meta-data to be displayed in the figure
+
+        The information provided in the parameter 'fig_info' will be displayed
+        in a small box.
+        """
+        if fig_info is None:
+            fig_info = FIGinfo()
+
+        # determine central longitude
+        if lons.max() - lons.min() > 180:
+            if np.sum(lons > 0) > np.sum(lons < 0):
+                lons[lons < 0] += 360
+            else:
+                lons[lons > 0] -= 360
+        lon_0 = np.around(np.mean(lons), decimals=-1)
+
+        # inititalize figure
+        fig = plt.figure(figsize=(12.5, 9))
+        if title is not None:
+            ypos = 1 - 0.35 / fig.get_figheight()
+            fig.suptitle(title, fontsize='x-large',
+                         position=(0.5, ypos), horizontalalignment='center')
+
+        # draw worldmap
+        axx = plt.axes(projection=BetterTransverseMercator(
+            central_longitude=lon_0, orientation=0,
+            globe=ccrs.Globe(ellipse='sphere')))
+        if sub_title is not None:
+            axx.set_title(sub_title, fontsize='large')
+        self.__draw_worldmap(axx, whole_globe=True)
 
         # draw footprint
         if sequence is None:
             lat = np.concatenate([lats[0, :], lats[1:-1, -1],
-                                  lats[-1, ::-1], lats[1:-1:-1, 0]])
+                                  lats[-1, ::-1], np.flipud(lats)[1:-1, 0]])
             lon = np.concatenate([lons[0, :], lons[1:-1, -1],
-                                  lons[-1, ::-1], lons[1:-1:-1, 0]])
+                                  lons[-1, ::-1], np.flipud(lons)[1:-1, 0]])
 
-            poly = Polygon(xy=list(zip(lon, lat)), closed=True,
-                           alpha=0.6, facecolor=self.cset['s5p'],
-                           transform=ccrs.PlateCarree())
+            poly = mpl.patches.Polygon(xy=list(zip(lon, lat)),
+                                       closed=True, alpha=0.6,
+                                       facecolor=self.cset['s5p'],
+                                       transform=ccrs.PlateCarree())
             axx.add_patch(poly)
         else:
+            cset = [tol_cset('bright').red, tol_cset('bright').purple]
             print('Unique sequence: {}'.format(np.unique(sequence)))
             for ii in np.unique(sequence):
                 indx = np.unique(np.where(sequence == ii)[0])
@@ -313,9 +421,10 @@ class S5Pgeoplot():
                                       lons[indx[-1], ::-1],
                                       lons[indx_rev, 0]])
 
-                poly = Polygon(xy=list(zip(lon, lat)), closed=True,
-                               alpha=0.6, facecolor=self.cset['s5p'],
-                               transform=ccrs.PlateCarree())
+                poly = mpl.patches.Polygon(xy=list(zip(lon, lat)),
+                                           closed=True, alpha=0.6,
+                                           facecolor=cset[ii % 2],
+                                           transform=ccrs.PlateCarree())
                 axx.add_patch(poly)
 
         self.__add_copyright(axx)
@@ -324,85 +433,33 @@ class S5Pgeoplot():
         self.__close_this_page(fig)
 
     # --------------------------------------------------
-    def draw_geo_subsat(self, lons, lats, *,
-                        title=None, fig_info=None):
-        """
-        Display sub-satellite coordinates projected with TransverseMercator
-
-        Parameters
-        ----------
-        lats         :  ndarray
-           Latitude coordinates
-        lons         :  ndarray
-           Longitude coordinates
-        title      :  string
-           Title of the figure. Default is None
-           Suggestion: use attribute "title" of data-product
-        fig_info :  FIGinfo, optional
-           OrderedDict holding meta-data to be displayed in the figure
-
-        The information provided in the parameter 'fig_info' will be displayed
-        in a small box.
-        """
-        if fig_info is None:
-            fig_info = FIGinfo()
-
-        # determine central longitude
-        if lons.max() - lons.min() > 180:
-            if np.sum(lons > 0) > np.sum(lons < 0):
-                lons[lons < 0] += 360
-            else:
-                lons[lons > 0] -= 360
-        lon_0 = np.around(np.mean(lons), decimals=-1)
-
-        # inititalize figure
-        fig = plt.figure(figsize=(15, 10))
-        if title is not None:
-            ypos = 1 - 0.25 / fig.get_figheight()
-            fig.suptitle(title, fontsize='x-large',
-                         position=(0.5, ypos), horizontalalignment='center')
-
-        # draw worldmap
-        axx = plt.axes(projection=BetterTransverseMercator(
-            central_longitude=lon_0, orientation=1,
-            globe=ccrs.Globe(ellipse='sphere')))
-        self.__draw_worldmap(axx)
-
-        # draw sub-satellite spot(s)
-        axx.scatter(lons, lats, 4, transform=ccrs.PlateCarree(),
-                    marker='o', color=self.cset['s5p'])
-
-        self.__add_copyright(axx)
-        fig_info.add('lon0', lon_0)
-        self.__add_fig_box(fig, fig_info)
-        self.__close_this_page(fig)
-
-    # --------------------------------------------------
-    def draw_geo_msm(self, gridlon, gridlat, msm_in, *,
-                     vperc=None, vrange=None,
-                     whole_globe=False, title=None, fig_info=None):
+    def draw_geo_msm(self, gridlon, gridlat, data_in, *,
+                     whole_globe=False, vperc=None, vrange=None,
+                     title=None, sub_title=None, fig_info=None):
         """
         Show measurement data projected with (better) TransverseMercator.
 
         Parameters
         ----------
-        gridlon   :  array_like
-        gridlat   :  array_like
-           The coordinates of the quadrilateral corners, with dimnsions one
+        gridlon :  array_like
+        gridlat :  array_like
+           The coordinates of the quadrilateral corners, with dimensions one
            larger then data
-        data      :  array_like
+        data :  array_like
            Measurement data as a scalar 2-D array. The values will be
            color-mapped.
-        vrange    :  list [vmin,vmax]
+        whole_globe :  boolean
+           Show the whole globe
+        vrange :  list [vmin,vmax]
            Range to normalize luminance data between vmin and vmax.
-        vperc     :  list
+        vperc :  list
            Range to normalize luminance data between percentiles min and max of
            array data. Default is [1., 99.].
            keyword 'vperc' is ignored when vrange is given
-        title     :  string
+        title :  string, optional
            Title of the figure. Default is None
            [Suggestion] use attribute "title" of data-product
-        sub_title :  string
+        sub_title :  string, optional
            Sub-title of the figure. Default is None
            [Suggestion] use attribute "comment" of data-product
         fig_info :  FIGinfo, optional
@@ -415,37 +472,31 @@ class S5Pgeoplot():
         if fig_info is None:
             fig_info = FIGinfo()
 
-        # assert that we have some data to show
-        if isinstance(msm_in, np.ndarray):
-            msm = S5Pmsm(msm_in)
-        else:
-            if not isinstance(msm_in, S5Pmsm):
-                raise TypeError('msm not an numpy.ndarray or pys5p.S5Pmsm')
-            msm = msm_in
-
-        if msm.value.ndim != 2:
-            raise ValueError('input data must be two dimensional')
-        if np.all(np.isnan(msm.value)):
-            raise ValueError('input data must contain valid data')
-        data = msm.value.copy()
-        if msm.units is None:
-            zunit = None
-        elif msm.units == '1':
-            zunit = None
-        else:
-            zunit = msm.units
-
-        # define data-range
-        if vrange is None:
-            if vperc is None:
-                vperc = (1., 99.)
-            else:
-                if len(vperc) != 2:
-                    raise TypeError('keyword vperc requires two values')
-            vmin, vmax = np.nanpercentile(data, vperc)
+        if vrange is None and vperc is None:
+            vperc = (1., 99.)
+        elif vrange is None:
+            if len(vperc) != 2:
+                raise TypeError('keyword vperc requires two values')
         else:
             if len(vrange) != 2:
                 raise TypeError('keyword vrange requires two values')
+
+        # check image data
+        try:
+            check_data2d('data', data_in)
+        except Exception as exc:
+            raise RuntimeError('invalid input-data provided') from exc
+
+        if isinstance(data_in, np.ndarray):
+            img_data = data_in.copy()
+        else:
+            img_data = data_in.value.copy()
+            self.set_zunit(data_in.units)
+
+        # define data-range
+        if vrange is None:
+            vmin, vmax = np.nanpercentile(img_data, vperc)
+        else:
             vmin, vmax = vrange
 
         # determine central longitude
@@ -461,12 +512,14 @@ class S5Pgeoplot():
                                           orientation=0,
                                           globe=ccrs.Globe(ellipse='sphere'))
 
-        fig, axx = plt.subplots(1, 1, figsize=(10, 12.5),
+        fig, axx = plt.subplots(1, 1, figsize=(12.5, 9),
                                 subplot_kw={'projection': myproj})
         if title is not None:
-            ypos = 1 - 0.25 / fig.get_figheight()
+            ypos = 1 - 0.35 / fig.get_figheight()
             fig.suptitle(title, fontsize='x-large',
                          position=(0.5, ypos), horizontalalignment='center')
+        if sub_title is not None:
+            axx.set_title(sub_title, fontsize='large')
 
         # Add the colorbar axes anywhere in the figure.
         # Its position will be re-calculated at each figure resize.
@@ -477,18 +530,17 @@ class S5Pgeoplot():
         self.__draw_worldmap(axx, whole_globe)
 
         # draw image and colorbar
-        img = axx.pcolormesh(gridlon, gridlat, data,
+        img = axx.pcolormesh(gridlon, gridlat, img_data,
                              vmin=vmin, vmax=vmax,
-                             cmap=tol_cmap('sunset'),
+                             cmap=self.get_cmap(),
                              transform=ccrs.PlateCarree())
         plt.draw()
         posn = axx.get_position()
         cax.set_position([posn.x0 + posn.width + 0.01,
                           posn.y0, 0.04, posn.height])
-        if zunit is None:
-            zlabel = 'value'
-        else:
-            zlabel = r'value [{}]'.format(zunit)
+
+        zlabel = 'value' if self.zunit is None \
+                 else r'value [{}]'.format(self.zunit)
         plt.colorbar(img, cax=cax, label=zlabel)
 
         # finalize figure
