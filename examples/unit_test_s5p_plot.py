f86bd3dddc725cbd5508a0a4c721273e7e434b3f
Richard van Hees <r.m.van.hees@sron.nl>
2021-01-05 17:58:42 +0100

    unit_test_s5p_plot.py: improved unit-test on s5p_plot

--- examples/unit_test_s5p_plot.py
+++ examples/unit_test_s5p_plot.py
@@ -19,86 +19,85 @@ from pys5p.s5p_msm import S5Pmsm
 from pys5p.s5p_plot import S5Pplot
 
 
+def get_msm(data_sel=None, xy_min=-5, xy_max=5, delta=0.01, error=0):
+    """
+    """
+    if data_sel is None:
+        data_sel = [(), ()]
+
+    res = np.arange(-5.0, 5.0, delta)
+    xmesh, ymesh = np.meshgrid(res[data_sel[1]], res[data_sel[0]])
+    zz1 = np.exp(-xmesh ** 2 - ymesh ** 2)
+    zz2 = np.exp(-(xmesh - 1) ** 2 - (ymesh - 1) ** 2)
+    data = (zz1 - zz2) * 2
+    data += np.random.default_rng().normal(0., error, data.shape)
+
+    msm = S5Pmsm(data)
+    msm.error = np.full(data.shape, error)
+    msm.set_units('Volt')
+    msm.set_long_name('bogus data')
+
+    return msm
+
+
 # --------------------------------------------------
 def run_draw_signal(plot):
     """
     Run unit tests on S5Pplot::draw_signal
     """
     print('Run unit tests on S5Pplot::draw_signal')
-    delta = 0.01
-    xx = yy = np.arange(-5.0, 5.0, delta)
-    xmesh, ymesh = np.meshgrid(xx, yy)
-    zz1 = np.exp(-xmesh ** 2 - ymesh ** 2)
-    zz2 = np.exp(-(xmesh - 1) ** 2 - (ymesh - 1) ** 2)
-    data = (zz1 - zz2) * 2
 
-    msm = S5Pmsm(data)
-    msm.set_units('Volt')
-    msm.set_long_name('bogus data')
-
-    plot.draw_signal(msm,
-                     title='Unit test of S5Pplot [draw_signal]',
-                     sub_title='aspect=1; fig_pos=above')
-
-    plot.draw_signal(data[500-250:500+250, :],
-                     title='Unit test of S5Pplot [draw_signal]',
-                     sub_title='aspect=2; fig_pos=above')
-
-    plot.set_zunit('electron.s-1')
-    plot.draw_signal(data[500-125:500+125, 500-375:500+375],
-                     title='Unit test of S5Pplot [draw_signal]',
-                     sub_title='aspect=3; fig_pos=above')
-
-    plot.unset_zunit()
-    plot.draw_signal(data[500-128:500+128, :],
-                     title='Unit test of S5Pplot [draw_signal]',
-                     sub_title='aspect=4; fig_pos=above')
+    msm = get_msm(error=.1)
+    msm_ref = get_msm(error=0.025)
 
     nlines = 40 # 35
     fig_info_in = FIGinfo('right')
     for ii in range(nlines):
         fig_info_in.add('line {:02d}'.format(ii), 5 * '0123456789')
-    plot.draw_signal(data, fig_info=fig_info_in.copy(),
+
+    plot.draw_signal(msm,
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='method=data; aspect=1; fig_pos=above')
+    plot.draw_signal(msm, msm_ref.value, method='diff',
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='method=diff; aspect=1; fig_pos=above')
+    plot.draw_signal(msm, msm_ref.value, method='ratio',
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='method=ratio; aspect=1; fig_pos=above')
+    plot.draw_signal(msm, msm_ref, method='ratio_unc',
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='method=ratio_unc; aspect=1; fig_pos=above')
+    plot.draw_signal(msm, method='error',
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='method=error; aspect=1; fig_pos=above')
+    plot.draw_signal(msm, fig_info=fig_info_in.copy(),
                      title='Unit test of S5Pplot [draw_signal]',
                      sub_title='aspect=1; fig_pos=right')
 
-    plot.draw_signal(data[500-250:500+250, :], fig_info=fig_info_in.copy(),
+    msm = get_msm(data_sel=np.s_[500-250:500+250, :], error=.1)
+    plot.draw_signal(msm,
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='aspect=2; fig_pos=above')
+    plot.draw_signal(msm, fig_info=fig_info_in.copy(),
                      title='Unit test of S5Pplot [draw_signal]',
                      sub_title='aspect=2; fig_pos=right')
 
-    plot.draw_signal(data[500-125:500+125, 500-375:500+375],
+    msm = get_msm(data_sel=np.s_[500-125:500+125, 500-375:500+375], error=.1)
+    plot.draw_signal(msm,
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='aspect=3; fig_pos=above')
+    plot.draw_signal(msm,
                      fig_info=fig_info_in.copy(),
                      title='Unit test of S5Pplot [draw_signal]',
                      sub_title='aspect=3; fig_pos=right')
 
-    plot.draw_signal(data[500-128:500+128, :], fig_info=fig_info_in.copy(),
+    msm = get_msm(data_sel=np.s_[500-128:500+128, :], error=.1)
+    plot.draw_signal(msm,
+                     title='Unit test of S5Pplot [draw_signal]',
+                     sub_title='aspect=4; fig_pos=above')
+    plot.draw_signal(msm, fig_info=fig_info_in.copy(),
                      title='Unit test of S5Pplot [draw_signal]',
                      sub_title='aspect=4; fig_pos=right')
-
-
-def run_draw_quality(plot):
-    """
-    Run unit tests on S5Pplot::draw_quality
-    """
-    print('Run unit tests on S5Pplot::draw_quality')
-    row = np.linspace(0, 1., 1000)
-    ref_data = np.repeat(row[None, :], 256, axis=0)
-
-    data = ref_data.copy()
-    data[125:175, 30:50] = 0.4
-    data[125:175, 50:70] = 0.95
-    data[75:125, 250:300] = 0.05
-    data[125:175, 600:650] = 0.95
-    data[75:125, 900:925] = 0.05
-    data[75:125, 825:875] = 0.4
-
-    plot.draw_quality(data,
-                      title='Unit test of S5Pplot [draw_quality]',
-                      sub_title='no reference')
-
-    plot.draw_quality(data, ref_data=ref_data,
-                      title='Unit test of S5Pplot [draw_quality]',
-                      sub_title='with reference')
 
 
 def run_draw_cmp_swir(plot):
@@ -106,23 +105,17 @@ def run_draw_cmp_swir(plot):
     Run unit tests on S5Pplot::draw_cmp_swir
     """
     print('Run unit tests on S5Pplot::draw_cmp_swir')
-    delta = 0.01
-    xx = yy = np.arange(-5.0, 5.0, delta)
-    xmesh, ymesh = np.meshgrid(xx, yy)
-    zz1 = np.exp(-xmesh ** 2 - ymesh ** 2)
-    zz2 = np.exp(-(xmesh - 1) ** 2 - (ymesh - 1) ** 2)
-    data = (zz1 - zz2) * 2
 
-    data = data[500-128:500+128, :]
-    ref_data = 1.001 * data
+    msm = get_msm(data_sel=np.s_[500-128:500+128, :], error=.1)
+    msm_ref = get_msm(data_sel=np.s_[500-128:500+128, :], error=.025)
 
-    plot.draw_cmp_swir(data, ref_data,
+    plot.draw_cmp_swir(msm, msm_ref.value,
                        title='Unit test of S5Pplot [draw_cmp_images]',
                        sub_title='test image')
-    plot.draw_cmp_swir(data, ref_data, add_residual=False,
+    plot.draw_cmp_swir(msm, msm_ref.value, add_residual=False,
                        title='Unit test of S5Pplot [draw_cmp_images]',
                        sub_title='test image')
-    plot.draw_cmp_swir(data, ref_data, add_model=False,
+    plot.draw_cmp_swir(msm, msm_ref.value, add_model=False,
                        title='Unit test of S5Pplot [draw_cmp_images]',
                        sub_title='test image')
 
@@ -211,6 +204,31 @@ def run_draw_lines(plot):
                     xlabel='x-axis', ylabel='y-axis',
                     sub_title='draw_lines [time_axis]')
 
+def run_draw_quality(plot):
+    """
+    Run unit tests on S5Pplot::draw_quality
+    """
+    print('Run unit tests on S5Pplot::draw_quality')
+    row = np.linspace(0, 1., 1000)
+    ref_data = np.repeat(row[None, :], 256, axis=0)
+
+    data = ref_data.copy()
+    data[125:175, 30:50] = 0.4
+    data[125:175, 50:70] = 0.95
+    data[75:125, 250:300] = 0.05
+    data[125:175, 600:650] = 0.95
+    data[75:125, 900:925] = 0.05
+    data[75:125, 825:875] = 0.4
+
+    plot.draw_quality(data,
+                      title='Unit test of S5Pplot [draw_quality]',
+                      sub_title='no reference')
+
+    plot.draw_quality(data, ref_data=ref_data,
+                      title='Unit test of S5Pplot [draw_quality]',
+                      sub_title='with reference')
+
+
 def run_draw_qhist(plot):
     """
     Run unit tests on S5Pplot::draw_qhist
@@ -250,23 +268,27 @@ def main():
     """
     plot = S5Pplot('unit_test_s5p_plot.pdf')
     check_draw_signal = True
-    check_draw_quality = True
     check_draw_cmp_images = True
-    check_draw_trend1d = True
-    check_draw_lines = True
-    check_draw_qhist = True
+    check_draw_quality = False
+    check_draw_qhist = False
+    check_draw_trend1d = False
+    check_draw_lines = False
 
     # ---------- UNIT TEST: draw_signal ----------
     if check_draw_signal:
         run_draw_signal(plot)
 
+    # ---------- UNIT TEST: draw_cmp_images ----------
+    if check_draw_cmp_images:
+        run_draw_cmp_swir(plot)
+
     # ---------- UNIT TEST: draw_quality ----------
     if check_draw_quality:
         run_draw_quality(plot)
 
-    # ---------- UNIT TEST: draw_cmp_images ----------
-    if check_draw_cmp_images:
-        run_draw_cmp_swir(plot)
+    # ---------- UNIT TEST: draw_qhist ----------
+    if check_draw_qhist:
+        run_draw_qhist(plot)
 
     # ---------- UNIT TEST: draw_lines ----------
     if check_draw_trend1d:
@@ -275,10 +297,6 @@ def main():
     # ---------- UNIT TEST: draw_lines ----------
     if check_draw_lines:
         run_draw_lines(plot)
-
-    # ---------- UNIT TEST: draw_qhist ----------
-    if check_draw_qhist:
-        run_draw_qhist(plot)
 
     # close figure
     plot.close()
