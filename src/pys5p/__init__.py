f062b4c735d8c957e0522466923ca60219939e47
Richard van Hees <r.m.van.hees@sron.nl>
2023-01-18 17:21:39 +0100

    Replaced version.get() by pys5p_version()

--- src/pys5p/__init__.py
+++ src/pys5p/__init__.py
@@ -3,17 +3,17 @@
 #
 # https://github.com/rmvanhees/pys5p.git
 #
-# Copyright (c) 2017-2021 SRON - Netherlands Institute for Space Research
+# Copyright (c) 2017-2023 SRON - Netherlands Institute for Space Research
 #   All Rights Reserved
 #
 # License:  BSD-3-Clause
 
 """ The pys5p package contains software to read S5p Tropomi L1B products.
-    And contains plotting routines to display your data beautifully."""
-
-from pkg_resources import get_distribution, DistributionNotFound
+And contains plotting routines to display your data beautifully.
+"""
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = version(__name__)
+except PackageNotFoundError:
     pass
--- src/pys5p/version.py
+++ src/pys5p/version.py
@@ -2,20 +2,24 @@
 #
 # https://github.com/rmvanhees/pys5p.git
 #
-# Copyright (c) 2017-2022 SRON - Netherlands Institute for Space Research
+# Copyright (c) 2017-2023 SRON - Netherlands Institute for Space Research
 #   All Rights Reserved
 #
 # License:  BSD-3-Clause
+"""Provide access to the software version as obtained from git.
 """
-Provide access to the software version as obtained from git.
-"""
+__all__ = ['pys5p_version']
+
 from pys5p import __version__
 
 
-def get(full=False):
+def pys5p_version(full=False, githash=False):
     """Returns software version as obtained from git.
     """
     if full:
         return __version__
+
+    if githash:
+        return __version__.split('+g')[1].split('.')[0]
 
     return __version__.split('+')[0]
